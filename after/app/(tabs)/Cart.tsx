a27f5431b279c64583ac8db0e070ad31e3785ffb
Amol Chitale <amolchitaledcjr@gmail.com>
2024-07-02 22:32:15 +0530

    UI Features

--- after/app/(tabs)/Cart.tsx
+++ after/app/(tabs)/Cart.tsx
@@ -1,3 +1,4 @@
+import CartCard from '@/components/CartCard';
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks';
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -10,22 +11,42 @@ export default function TabTwoScreen() {
 
 
   return (
-    <View>
+    <View style={styles.appContainer}>
       {
         cart.cart.length == 0?<Text style={styles.text}>
           Please Add something to your cart
-        </Text>:<></>
+        </Text>:<View>
+          <Text style={styles.text}>
+            Current Cart
+            <View style={styles.container}>
+
+            {
+              cart.cart.map((item)=><CartCard pizza={item}/>)
+            }
+            </View>
+          </Text>
+          
+        </View>
       }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  appContainer: {
+    padding: 10,
+    gap: 40
+  },
  text:{
     fontSize: 20,
     color: '#FFFFFF',
     fontWeight: "bold",
     top: 40,
     alignSelf: "center",
+ },
+ container:{
+   top:100,
+   gap: 20,
+   color: "#ffffff"
  }
 });
---
+++ after/components/CartCard.tsx
@@ -0,0 +1,15 @@
+import { CartItem } from "@/types"
+import { Text, View } from "react-native"
+
+
+const CartCard = (pizza:CartItem|any) => {
+  return (
+    <View>
+        <Text>
+
+        </Text>
+    </View>
+  )
+}
+
+export default CartCard
